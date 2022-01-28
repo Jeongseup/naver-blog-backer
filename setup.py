075f4cae98767675cddb3e0d43d2618c817ed1f2
Jeongseup <thswjdtmq4@inu.ac.kr>
2022-01-28 20:33:52 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
-setuptools.setup(
-    name="naver 이름",
+setup(
+    name="naver-blog-backer",
     version="0.1",
     license='MIT',
-    author="패키지 제작자 이름",
-    author_email="패키지 제작자 이메일",
-    description="패키지 요약",
+    description="for your naver blog backup and backlinks",
     long_description=open('README.md').read(),
-    url="github url 등",
-    packages=setuptools.find_packages(),
+    keywords=['naver','naver blog','blog crawling', 'blog backup', 'blog backlink'],
+
+    author="Jeongseup",
+    author_email="thswjdtmq4@inu.ac.kr",
+    url="https://github.com/Jeongseup/naver-blog-backer",
+    download_url="https://github.com/Jeongseup/naver-blog-backer/archive/main.zip",
+
+    install_requires = ['tqdm', 'beautifulsoup4', 'requests'],
+    packages=find_packages(exclude=[]),
+    python_requires = '>=3',
     classifiers=[
-        # 패키지에 대한 태그
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Windows OS"
     ],
 )

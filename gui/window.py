c01a69535720ebc561edb3854d74f70516697d52
Jeongseup <thswjdtmq4@inu.ac.kr>
2022-01-31 23:43:11 +0900

    gui programming2

--- gui/window.py
+++ gui/window.py
@@ -1,7 +1,9 @@
 import sys, os
-from PyQt5.QtWidgets import QApplication, QTextEdit, QFileDialog, QDesktopWidget, QGridLayout, \
-	QLabel, QLineEdit, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
+
+from PyQt5.QtCore import QFile
+from PyQt5.QtWidgets import QApplication, QFileDialog, QDesktopWidget, QGridLayout, \
+	QLabel, QLineEdit, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtGui import QIcon, QFont
 
 
 class MyApp(QWidget):
@@ -11,31 +13,30 @@ class MyApp(QWidget):
 		self.initUI()
 
 	def initUI(self):
-
 		# first widget
 		self.idLine = QLineEdit()
 		self.idLine.returnPressed.connect(self.enterId)
 
 		# second widget
-		self.savePathBtn = QPushButton('Select Empty Folder')
-		self.savePathBtn.pressed.connect(self.showDialog)
+		savePathBtn = QPushButton('Select Empty Folder')
+		savePathBtn.pressed.connect(self.showDialog)
 
 		# third widget
-		self.resultLbl1 = QLabel('Entered your naver id is ...')
+		self.settingLable1 = QLabel('현재 입력된 아이디는 ...asddadsad ')
 
 		grid = QGridLayout()
 		grid.addWidget(QLabel('Enter your naver ID : '), 0, 0)
 		grid.addWidget(QLabel('Choose save directory :'), 1, 0)
-		grid.addWidget(QLabel('RESULT:'), 2, 0)
+		grid.addWidget(QLabel('Current Status:'), 2, 0)
 
 		grid.addWidget(self.idLine, 0, 1)
-		grid.addWidget(self.savePathBtn, 1, 1)
-		grid.addWidget(self.resultLbl1, 2, 1)
+		grid.addWidget(savePathBtn, 1, 1)
+		grid.addWidget(self.settingLable1, 2, 1)
 
 		self.setLayout(grid)
 		self.setWindowIcon(QIcon('./src/icon.png'))
 		self.setWindowTitle('Naver Blog Backer')
-		self.setGeometry(300, 300, 300, 200)
+		self.resize(600, 400)
 		self.center()
 		self.show()
 
@@ -45,19 +46,12 @@ class MyApp(QWidget):
 	# btn = QPushButton('Quit', self)
 
 	def enterId(self):
-		print('enter id')
+		text = self.idLine.text()
 
-	def append_text(self):
-		text = self.le.text()
-		self.tb.append(text)
-		self.le.clear()
+		print('현재 입력된 텍스트 :', text)
 
-	def clear_text(self):
-		self.tb.clear()
-
-	def text_changed(self):
-		text = self.te.toPlainText()
-		self.lbl2.setText('The number of words is ' + str(len(text.split())))
+		self.settingLable1.setText(f'현재 입력된 아이디는 {text}')
+		self.settingLable1.adjustSize()
 
 	def center(self):
 		qr = self.frameGeometry()
@@ -76,14 +70,16 @@ class MyApp(QWidget):
 			print('no select')
 		else:
 			pass
-	# def text_changed(self):
-	#
-	#     text = self.te.toPlainText()
-	#     self.lbl2.setText('The number of words is ' + str(len(text.split())))
 
+def load_stylesheet(app, res="./src/style.qss"):
+        rc = QFile(res)
+        rc.open(QFile.ReadOnly)
+        content = rc.readAll().data()
+        app.setStyleSheet(str(content, "utf-8"))
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
+	load_stylesheet(app)
+	app.setFont(QFont('엘리스 디지털배움체 OTF'))
 	ex = MyApp()
-
 	sys.exit(app.exec_())

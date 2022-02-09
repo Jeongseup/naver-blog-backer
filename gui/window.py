339613e76a86ee1045277acf706dfdc40f154cd6
Jeongseup <thswjdtmq4@inu.ac.kr>
2022-02-09 16:22:07 +0900

    add radiobutton

--- gui/window.py
+++ gui/window.py
@@ -1,21 +1,48 @@
 import sys, os
-
 from PyQt5.QtCore import QFile
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication, QFileDialog, QDesktopWidget, QGridLayout, \
-	QLabel, QLineEdit, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+	QLabel, QLineEdit, QWidget, QPushButton, QRadioButton
 from PyQt5.QtGui import QIcon, QFont
 
 
-# from naverblogbacker.blog import BlogPost
+def load_stylesheet(app, res="./src/style.qss"):
+	rc = QFile(res)
+	rc.open(QFile.ReadOnly)
+	content = rc.readAll().data()
+	app.setStyleSheet(str(content, "utf-8"))
+
 
 class MyApp(QWidget):
 
 	def __init__(self):
 		super().__init__()
+		self.myId = None
+		self.myOption = None
+
 		self.initUI()
 
 	def initUI(self):
-		# first widget
+		grid = QGridLayout()
+
+		grid.addWidget(QLabel('Choose one option'), 0, 0)
+
+		radioButton = QRadioButton('[옵션1] 백업')
+		radioButton.option = "backup"
+		radioButton.toggled.connect(self.onClicked)
+		grid.addWidget(radioButton, 0, 1)
+
+		radioButton = QRadioButton('[옵션2] 백링크')
+		radioButton.option = "backlink"
+		radioButton.toggled.connect(self.onClicked)
+		grid.addWidget(radioButton, 0, 2)
+
+		radioButton = QRadioButton('[옵션3] 모두')
+		radioButton.option = "both"
+		radioButton.toggled.connect(self.onClicked)
+		grid.addWidget(radioButton, 0, 3)
+
+		#  widget
 		self.idLine = QLineEdit()
 		self.idLine.returnPressed.connect(self.enterId)
 
@@ -26,24 +53,33 @@ class MyApp(QWidget):
 		# third widget
 		self.settingLable1 = QLabel('하단에서 당신의 아이디를 입력해주세요 :)')
 
+		# buttons
+		okButton = QPushButton('OK')
+
+		cancelButton = QPushButton('Cancel')
+		cancelButton.clicked.connect(QCoreApplication.instance().quit)
+
 		# ========================================================================
 
-		grid = QGridLayout()
-		grid.addWidget(QLabel('Current Status:'), 0, 0)
-		grid.addWidget(self.settingLable1, 0, 1)
 
-		grid.addWidget(QLabel('Enter your naver ID : '), 1, 0)
-		grid.addWidget(self.idLine, 1, 1)
 
-		grid.addWidget(QLabel('Choose save directory :'), 2, 0)
-		grid.addWidget(savePathBtn, 2, 1)
+		grid.addWidget(QLabel('Current Status:'), 1, 0)
+		grid.addWidget(self.settingLable1, 1, 1)
+
+		grid.addWidget(QLabel('Enter your naver ID : '), 2, 0)
+		grid.addWidget(self.idLine, 2, 1)
+
+		grid.addWidget(QLabel('Choose save directory :'), 3, 0)
+		grid.addWidget(savePathBtn, 3, 1)
+
+		grid.addWidget(okButton, 4, 0)
+		grid.addWidget(cancelButton, 4, 1)
 
 		# ========================================================================
 
 		self.setLayout(grid)
 		self.setWindowIcon(QIcon('./src/icon.png'))
 		self.setWindowTitle('Naver Blog Backer')
-		self.resize(600, 400)
 		self.center()
 		self.show()
 
@@ -52,6 +88,13 @@ class MyApp(QWidget):
 	# openFile.triggered.connect(self.showDialog)
 	# btn = QPushButton('Quit', self)
 
+	def onClicked(self, option):
+		radioButton = self.sender()
+		if radioButton.isChecked():
+			print(f'Click the {radioButton.option}')
+			self.myOption = radioButton.option
+
+
 	def enterId(self):
 		text = self.idLine.text()
 
@@ -59,6 +102,9 @@ class MyApp(QWidget):
 
 		self.settingLable1.setText(f'현재 입력된 아이디는 "{text}" 입니다.')
 		self.settingLable1.adjustSize()
+
+	def enterOption(self):
+		print(self)
 
 	def center(self):
 		qr = self.frameGeometry()
@@ -78,15 +124,10 @@ class MyApp(QWidget):
 		else:
 			pass
 
-def load_stylesheet(app, res="./src/style.qss"):
-        rc = QFile(res)
-        rc.open(QFile.ReadOnly)
-        content = rc.readAll().data()
-        app.setStyleSheet(str(content, "utf-8"))
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	load_stylesheet(app)
-	app.setFont(QFont('엘리스 디지털배움체 OTF'))
+	# load_stylesheet(app)
+	# app.setFont(QFont('엘리스 디지털배움체 OTF'))
 	ex = MyApp()
 	sys.exit(app.exec_())

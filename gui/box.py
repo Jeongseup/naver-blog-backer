49ce9160db3e3cc8947c7193faa6b0c23a2080eb
Jeongseup <thswjdtmq4@inu.ac.kr>
2022-02-10 18:35:35 +0900

    devide by groupboxes

--- gui/box.py
+++ gui/box.py
@@ -1,11 +1,8 @@
 import sys
 
 from PyQt5.QtCore import QCoreApplication, QFile
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox
-
-
-# main(myId, myPath, myOption)
+from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox, QLineEdit
 
 class MyApp(QWidget):
 
@@ -13,28 +10,72 @@ class MyApp(QWidget):
 		super().__init__()
 
 		self.myId = None
-		self.myOption = None
 		self.myPath = None
+		self.myOption = None
 
 		self.initUI()
 
 	def initUI(self):
 
-		vbox = QVBoxLayout()
-		vbox.addStretch(1)
-		vbox.addWidget(self.optionGroup())
-		vbox.addStretch(1)
-		# vbox.addWidget(self.statusGroup())
-		vbox.addStretch(1)
+		vBox = QVBoxLayout()
+		vBox.addStretch(1)
+		vBox.addWidget(self.optionGroup())
+		vBox.addStretch(1)
+		vBox.addWidget(self.statusGroup())
+		vBox.addStretch(1)
 
-		self.setLayout(vbox)
-		self.setWindowTitle('Box Layout')
-		self.setGeometry(300, 300, 300, 200)
+		self.setLayout(vBox)
+		self.setWindowIcon(QIcon('./src/icon.png'))
+		self.setWindowTitle('NAVER BLOG BACKER')
+		# self.setGeometry(300, 300, 300, 200)
 		self.show()
 
+	# ==============================================================
+	def statusGroup(self):
+		groupBox = QGroupBox('현재 옵션')
+		# groupBox.setFlat(True)
+
+		vBox = QVBoxLayout()
+		vBox.addLayout(self.statusBox())
+
+		groupBox.setLayout(vBox)
+		return groupBox
+
+	def statusBox(self):
+		label1 = QLabel('Status Label', self)
+		label1.setAlignment(Qt.AlignCenter)
+
+		label2 = QLabel('Second Label', self)
+		label2.setAlignment(Qt.AlignVCenter)
+
+		lbl_red.setStyleSheet("color: red;"
+							  "border-style: solid;"
+							  "border-width: 2px;"
+							  "border-color: #FA8072;"
+							  "border-radius: 3px")
+
+		lbl_green.setStyleSheet("color: green;"
+								"background-color: #7FFFD4")
+
+		font1 = label1.font()
+		font1.setPointSize(20)
+
+		idLabel = QLabel('네이버 아이디를 입력하세요 : ')
+		inputIdLine = QLineEdit()
+		inputIdLine.returnPressed.connect(self.enterId)
+
+		hBox = QHBoxLayout()
+		hBox.addStretch(1)
+		hBox.addWidget(idLabel)
+		hBox.addWidget(inputIdLine)
+		hBox.addStretch(1)
+
+		return hBox
+
+	# ==============================================================
 	def optionGroup(self):
 		groupBox = QGroupBox('옵션들')
-		groupBox.setFlat(True)
+		# groupBox.setFlat(True)
 
 		vBox = QVBoxLayout()
 		vBox.addLayout(self.idBox())
@@ -58,14 +99,29 @@ class MyApp(QWidget):
 		return hBox
 
 	def idBox(self):
-		idLable = QLabel('아이디를 입력하세요 : ')
+		idLabel = QLabel('네이버 아이디를 입력하세요 : ')
+		inputIdLine = QLineEdit()
+		inputIdLine.returnPressed.connect(self.enterId)
 
 		hBox = QHBoxLayout()
 		hBox.addStretch(1)
-		hBox.addWidget(idLable)
+		hBox.addWidget(idLabel)
+		hBox.addWidget(inputIdLine)
 		hBox.addStretch(1)
 
 		return hBox
+
+	# ==============================================================
+
+	def enterId(self):
+		inputIdLine = self.sender()
+		inputId = inputIdLine.text()
+
+		print(f' [DEV] User entered the id, {inputId}')
+		self.myId = inputId
+
+		# self.settingLable1.setText(f'현재 입력된 아이디는 "{text}" 입니다.')
+		# self.settingLable1.adjustSize()
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
--- gui/group.py
+++ gui/group.py
@@ -1,7 +1,7 @@
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QGroupBox, QRadioButton
-, QCheckBox, QPushButton, QMenu, QGridLayout, QVBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget
+, QLineEdit, QTextBrowser, QPushButton, QVBoxLayout)
 
 
 class MyApp(QWidget):
@@ -11,103 +11,34 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        grid = QGridLayout()
-        grid.addWidget(self.createFirstExclusiveGroup(), 0, 0)
-        grid.addWidget(self.createSecondExclusiveGroup(), 1, 0)
-        grid.addWidget(self.createNonExclusiveGroup(), 0, 1)
-        grid.addWidget(self.createPushButtonGroup(), 1, 1)
+        self.le = QLineEdit()
+        self.le.returnPressed.connect(self.append_text)
 
-        self.setLayout(grid)
+        self.tb = QTextBrowser()
+        self.tb.setAcceptRichText(True)
+        self.tb.setOpenExternalLinks(True)
 
-        self.setWindowTitle('Box Layout')
-        self.setGeometry(300, 300, 480, 320)
+        self.clear_btn = QPushButton('Clear')
+        self.clear_btn.pressed.connect(self.clear_text)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.le, 0)
+        vbox.addWidget(self.tb, 1)
+        vbox.addWidget(self.clear_btn, 2)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QTextBrowser')
+        self.setGeometry(300, 300, 300, 300)
         self.show()
 
-    def createFirstExclusiveGroup(self):
-        groupbox = QGroupBox('Exclusive Radio Buttons')
+    def append_text(self):
+        text = self.le.text()
+        self.tb.append(text)
+        self.le.clear()
 
-        radio1 = QRadioButton('Radio1')
-        radio2 = QRadioButton('Radio2')
-        radio3 = QRadioButton('Radio3')
-        radio1.setChecked(True)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        groupbox.setLayout(vbox)
-
-        return groupbox
-
-    def createSecondExclusiveGroup(self):
-        groupbox = QGroupBox('Exclusive Radio Buttons')
-        groupbox.setCheckable(True)
-        groupbox.setChecked(False)
-
-        radio1 = QRadioButton('Radio1')
-        radio2 = QRadioButton('Radio2')
-        radio3 = QRadioButton('Radio3')
-        radio1.setChecked(True)
-        checkbox = QCheckBox('Independent Checkbox')
-        checkbox.setChecked(True)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(radio1)
-        vbox.addWidget(radio2)
-        vbox.addWidget(radio3)
-        vbox.addWidget(checkbox)
-        vbox.addStretch(1)
-        groupbox.setLayout(vbox)
-
-        return groupbox
-
-    def createNonExclusiveGroup(self):
-        groupbox = QGroupBox('Non-Exclusive Checkboxes')
-        groupbox.setFlat(True)
-
-        checkbox1 = QCheckBox('Checkbox1')
-        checkbox2 = QCheckBox('Checkbox2')
-        checkbox2.setChecked(True)
-        tristatebox = QCheckBox('Tri-state Button')
-        tristatebox.setTristate(True)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(checkbox1)
-        vbox.addWidget(checkbox2)
-        vbox.addWidget(tristatebox)
-        vbox.addStretch(1)
-        groupbox.setLayout(vbox)
-
-        return groupbox
-
-    def createPushButtonGroup(self):
-        groupbox = QGroupBox('Push Buttons')
-        groupbox.setCheckable(True)
-        groupbox.setChecked(True)
-
-        pushbutton = QPushButton('Normal Button')
-        togglebutton = QPushButton('Toggle Button')
-        togglebutton.setCheckable(True)
-        togglebutton.setChecked(True)
-        flatbutton = QPushButton('Flat Button')
-        flatbutton.setFlat(True)
-        popupbutton = QPushButton('Popup Button')
-        menu = QMenu(self)
-        menu.addAction('First Item')
-        menu.addAction('Second Item')
-        menu.addAction('Third Item')
-        menu.addAction('Fourth Item')
-        popupbutton.setMenu(menu)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(pushbutton)
-        vbox.addWidget(togglebutton)
-        vbox.addWidget(flatbutton)
-        vbox.addWidget(popupbutton)
-        vbox.addStretch(1)
-        groupbox.setLayout(vbox)
-
-        return groupbox
+    def clear_text(self):
+        self.tb.clear()
 
 
 if __name__ == '__main__':
